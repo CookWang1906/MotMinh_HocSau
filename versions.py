99efe9c09e1c60b62a18281c8037e27b31d6899b
qh123 <qhoang1950@gmail.com>
2026-09-16 09:48:23 +0700

    Thu vien duoc su dung trong repo

---
+++ versions.py
@@ -0,0 +1,20 @@
+# Import the libraries
+import scipy
+import numpy
+import matplotlib
+import pandas
+import statsmodels
+import sklearn
+import torch
+import torchvision
+
+print("Python library versions:")
+print('- scipy: %s' % scipy.__version__)
+print('- numpy: %s' % numpy.__version__)
+print('- matplotlib: %s' % matplotlib.__version__)
+print('- pandas: %s' % pandas.__version__)
+print('- statsmodels: %s' % statsmodels.__version__)
+print('- sklearn: %s' % sklearn.__version__)
+print("Deep learning library versions:")
+print('- pytorch: %s' % torch.__version__)
+print('- torchvision: %s' % torchvision.__version__)
